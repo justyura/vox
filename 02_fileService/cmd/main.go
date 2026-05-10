fbc502ad6925bc76b1a89591ffc550014dee67ce
TheYura <me@wtyura.com>
2026-05-10 20:44:20 +0800

    refactor: seperate the file manager service from main.go

--- 02_fileService/cmd/main.go
+++ 02_fileService/cmd/main.go
@@ -7,51 +7,15 @@ import (
 	"log"
 	"os"
 	"os/signal"
-	"time"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 
 	"github.com/google/uuid"
-	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
+	"github.com/justyura/vox/02_fileService/internal/blob"
+	"github.com/justyura/vox/02_fileService/internal/meta"
 	"github.com/justyura/vox/02_fileService/internal/migrations"
-	"github.com/justyura/vox/02_fileService/internal/model"
-	"github.com/minio/minio-go/v7"
-	"github.com/minio/minio-go/v7/pkg/credentials"
-	"github.com/minio/minio-go/v7/pkg/notification"
 )
-
-type FileServer struct {
-	api  *minio.Client
-	conn *pgx.Conn
-}
-
-func NewMinioClient(endpoint, accessKey, secretAccessKey string) (*minio.Client, error) {
-	api, err := minio.New(endpoint, &minio.Options{
-		Creds:  credentials.NewStaticV4(accessKey, secretAccessKey, ""),
-		Secure: false,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("create minio client: %w", err)
-	}
-	return api, nil
-}
-
-func NewDatabaseConn(databaseurl string) (*pgx.Conn, error) {
-	conn, err := pgx.Connect(context.Background(), databaseurl)
-	if err != nil {
-		return nil, fmt.Errorf("Unable to connect to database: %w", err)
-	}
-
-	return conn, nil
-}
-
-func NewFileServer(api *minio.Client, conn *pgx.Conn) *FileServer {
-	return &FileServer{
-		api:  api,
-		conn: conn,
-	}
-}
 
 func main() {
 	loadEnv()
@@ -67,11 +31,11 @@ func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer stop()
 
-	minioApi, err := NewMinioClient(os.Getenv("MINIO_ENDPOINT"), os.Getenv("MINIO_ACCESSKEY"), os.Getenv("MINIO_SECRETACCESSKEY"))
+	minioApi, err := blob.NewMinioClient(os.Getenv("MINIO_ENDPOINT"), os.Getenv("MINIO_ACCESSKEY"), os.Getenv("MINIO_SECRETACCESSKEY"))
 	if err != nil {
 		log.Fatalln(err)
 	}
-	dbConn, err := NewDatabaseConn(os.Getenv("DATABASE_URL"))
+	dbConn, err := meta.NewPostgres(os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -114,81 +78,4 @@ func loadEnv() {
 	if err := godotenv.Load(); err != nil {
 		log.Println("no .env file found, using existing env vars")
 	}
-}
-
-func healthCheck(ctx context.Context, fs *FileServer) error {
-	if _, err := fs.api.ListBuckets(ctx); err != nil {
-		return fmt.Errorf("minio: %w", err)
-	}
-
-	if err := fs.conn.Ping(ctx); err != nil {
-		return fmt.Errorf("postgres: %w", err)
-	}
-	return nil
-}
-
-func (fs *FileServer) Upload(ctx context.Context, user string, filename string) (string, error) {
-	// TODO: DB create record: id, filename, owner,
-	fileid := uuid.New()
-	_, err := fs.conn.Exec(ctx, "INSERT INTO files (file_id, owner, filename, status) VALUES ($1, $2, $3, 'pending')", fileid, user, filename)
-	if err != nil {
-		return "", fmt.Errorf("create file record: %w", err)
-	}
-
-	// oss TODO: abstract this to a package future
-	link, err := fs.api.PresignedPutObject(ctx, "vox", fileid.String(), time.Hour)
-	if err != nil {
-		return "", fmt.Errorf("upload link created err: %w", err)
-	}
-	return link.String(), nil
-}
-
-func (fs *FileServer) ListenUpload(ctx context.Context, bucket string) {
-	ch := fs.api.ListenBucketNotification(ctx, "vox", "", "", []string{
-		string(notification.ObjectCreatedPut),
-	})
-	for message := range ch {
-		if message.Err != nil {
-			log.Println(message.Err)
-			continue
-		}
-		for _, event := range message.Records {
-			id := event.S3.Object.Key
-			size := event.S3.Object.Size
-			_, err := fs.conn.Exec(ctx, "UPDATE files SET status = 'ready', size = $2 WHERE file_id = $1", id, size)
-			if err != nil {
-				log.Println(err)
-			}
-		}
-
-	}
-}
-
-func (fs *FileServer) Listfiles(ctx context.Context, owner uuid.UUID) ([]model.File, error) {
-	rows, err := fs.conn.Query(ctx, "SELECT file_id, owner, filename, status, size, created_at FROM files WHERE owner=$1", owner)
-	if err != nil {
-		return nil, fmt.Errorf("listfiles: %w", err)
-	}
-	defer rows.Close()
-
-	files := make([]model.File, 0)
-	for rows.Next() {
-		var f model.File
-		if err := rows.Scan(&f.FileID, &f.Owner, &f.FileName, &f.Status, &f.Size, &f.CreatedAt); err != nil {
-			return nil, fmt.Errorf("db scan: %w", err)
-		}
-		files = append(files, f)
-	}
-	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("row iteration: %w", err)
-	}
-	return files, nil
-}
-
-func (fs *FileServer) Download(ctx context.Context, fileid uuid.UUID) (string, error) {
-	url, err := fs.api.PresignedGetObject(ctx, "vox", fileid.String(), time.Hour, nil)
-	if err != nil {
-		return "", fmt.Errorf("download link generated failed: %w", err)
-	}
-	return url.String(), nil
 }
---
+++ 02_fileService/internal/service/file.go
@@ -0,0 +1,58 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/google/uuid"
+	"github.com/justyura/vox/02_fileService/internal/blob"
+	"github.com/justyura/vox/02_fileService/internal/meta"
+	"github.com/justyura/vox/02_fileService/internal/model"
+)
+
+type FileServer struct {
+	oss   blob.OSS
+	store meta.Store
+}
+
+func NewFileServer(oss blob.OSS, store meta.Store) *FileServer {
+	return &FileServer{
+		oss:   oss,
+		store: store,
+	}
+}
+
+func (fs *FileServer) Upload(ctx context.Context, user uuid.UUID, filename string) (string, error) {
+	f := &model.File{
+		FileID:   uuid.New(),
+		Owner:    user,
+		FileName: filename,
+	}
+
+	if err := fs.store.Create(ctx, f); err != nil {
+		return "", fmt.Errorf("create file record: %w", err)
+	}
+
+	if link, err := fs.oss.Upload(ctx, f.FileID.String()); err != nil {
+		return "", fmt.Errorf("upload link created err: %w", err)
+	} else {
+		return link, nil
+	}
+}
+
+func (fs *FileServer) ListenUpload(ctx context.Context) {
+	for ev := range fs.oss.ListenUpload(ctx) {
+		if ev.Err != nil {
+			log.Println(ev.Err)
+			continue
+		}
+		if err := fs.store.MarkReady(ctx, ev.ID, ev.Size); err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func (fs *FileServer) Listfiles(ctx context.Context, owner uuid.UUID) ([]model.File, error) {
+	return fs.store.List(ctx, owner)
+}
