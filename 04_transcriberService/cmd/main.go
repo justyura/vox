6363035947e5806f1f4320ba5148be25b6042e30
TheYura <me@wtyura.com>
2026-06-14 22:33:12 +0800

    success download from one url

--- 04_transcriberService/cmd/main.go
+++ 04_transcriberService/cmd/main.go
@@ -1,14 +1,44 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/justyura/vox/04_workerService/internal/processor"
+	"io"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-	transcriber := &processor.Transcriber{}
+	// TODO: run transcriber basics
 
-	result, _ := transcriber.Process("a task")
-	fmt.Println(result)
+	if err := DownloadFromURL("http://103.170.232.132:8080"); err != nil {
+		log.Fatal("failed to download")
+	}
+
+	// result, err := Transcribe()
+	// if err != nil {
+	// 	fmt.Println(result)
+	// }
+}
+
+func DownloadFromURL(url string) error {
+	tmpfile, err := os.Create("output")
+	if err != nil {
+		return err
+	}
+	defer tmpfile.Close()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(tmpfile, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	log.Println("success download from url")
+
+	return nil
 }
